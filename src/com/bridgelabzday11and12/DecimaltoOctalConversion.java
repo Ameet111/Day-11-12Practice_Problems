b9712b63664def2d1414db626d5ebe78bd5cef3f
Ameet Jadhav <ammugs11@gmail.com>
2022-09-06 17:46:26 +0530

    added Decimal to Octal Conversion

---
+++ src/com/bridgelabzday11and12/DecimaltoOctalConversion.java
@@ -0,0 +1,34 @@
+package com.bridgelabzday11and12;
+
+import java.util.Scanner;
+
+public class DecimaltoOctalConversion {
+	
+	 public static void main(String args[])
+	  {
+	    Scanner input = new Scanner( System.in );
+	    System.out.print("Enter a decimal number : ");
+	    int num =input.nextInt();
+	 
+	    String octalString = Integer.toOctalString(num);
+	    System.out.println("Method 1: Decimal to octal: " + octalString);
+	 
+	    // For storing remainder
+	    int rem;
+	 
+	    // For storing result
+	    String str=""; 
+	 
+	    // Digits in Octal number system
+	    char dig[]={'0','1','2','3','4','5','6','7'};
+	 
+	    while(num>0)
+	    {
+	       rem=num%8; 
+	       str=dig[rem]+str; 
+	       num=num/8;
+	    }
+	    System.out.println("Method 2: Decimal to octal: "+str);
+	  }
+
+}
